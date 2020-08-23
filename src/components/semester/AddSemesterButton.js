9a125b04a504fc5ebdab61c72863cffa021b17f6
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-08-23 19:06:06 +0100

    fix: console warnings

--- src/components/semester/AddSemesterButton.js
+++ src/components/semester/AddSemesterButton.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addSemester as addSemesterBelow } from "../../redux/actions/componentActivity";
 
 const AddSemesterButton = ({ semestersLength, level }) => {
   const dispatch = useDispatch();
-  const activeLevel = useSelector(
-    (state) => state.componentActivity.activeLevel
-  );
   const addSemester = () => {
     dispatch(addSemesterBelow(level));
   };
