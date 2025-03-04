2c0814b2af21a5cd09dfa42d94bfb38ebb1abcb2
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2025-03-04 09:04:59 +0000

    chore: update react core dependencies

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,16 @@
-import ReactLoader from "react-loader-spinner";
+// import ReactLoader from "react-loader-spinner";
 
 function Loader({ loading }) {
   return (
     loading && (
       <div className="d-flex justify-content-center py-3">
-        <ReactLoader
+        loading... lol
+        {/* <ReactLoader
           type="TailSpin"
           className="text-primary"
           height={50}
           width={50}
-        />
+        /> */}
       </div>
     )
   );
